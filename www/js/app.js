6af203635844566011b35938f531714ce9642978
Jan Sourek <jan.sourek@gmail.com>
2013-10-27 22:29:22 +0100

    phonegap.js

--- www/js/app.js
+++ www/js/app.js
@@ -1,33 +1,36 @@
-require.config({
 
-    baseUrl: 'js/lib',
-
-    paths: {
-        app: '../app',
-        tpl: '../tpl'
-    },
-
-    map: {
-        '*': {
-            'app/models/employee': 'app/models/memory/employee'
-        }
-    },
-
-    shim: {
-        'backbone': {
-            deps: ['underscore', 'jquery'],
-            exports: 'Backbone'
-        },
-        'underscore': {
-            exports: '_'
-        },
-        'bootstrap/bootstrap': {
-        	deps: ['jquery'],
-        }
-    }
-});
-
-require(['jquery', 'backbone', 'app/router'], function ($, Backbone, Router) {
-    var router = new Router();
-    Backbone.history.start();
-});
+    
+		require.config({
+		
+		    baseUrl: 'js/lib',
+		
+		    paths: {
+		        app: '../app',
+		        tpl: '../tpl'
+		    },
+		
+		    map: {
+		        '*': {
+		            'app/models/employee': 'app/models/memory/employee'
+		        }
+		    },
+		
+		    shim: {
+		        'backbone': {
+		            deps: ['underscore', 'jquery'],
+		            exports: 'Backbone'
+		        },
+		        'underscore': {
+		            exports: '_'
+		        },
+		        'bootstrap/bootstrap': {
+		        	deps: ['jquery'],
+		        }
+		    }
+		});
+		
+		require(['jquery', 'backbone', 'app/router'], function ($, Backbone, Router) {
+		    var router = new Router();
+		    Backbone.history.start();
+		});
+		
